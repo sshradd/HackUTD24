efbcee0be5a94966666678307d4bc93c1197da55
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-11-16 23:45:38 -0600

    onboarding done

--- blossum/src/app/onboarding/financebg/page.tsx
+++ blossum/src/app/onboarding/financebg/page.tsx
@@ -9,39 +9,39 @@ const FinanceBG = () => {
         <div className="flex flex-col">
           <Image src="/tree.png" alt="Welcome" width={800} height={400} />
         </div>
-        <div className="flex flex-col ml-52 space-y-8">
+        <div className="flex flex-col ml-52">
           <div className="flex items-center flex-col">
             <h1 className="text-5xl font-bold text-wenge mb-8">Financial Background</h1>
             <div className="flex flex-col space-y-3">
-              <p className="text-2xl text-wenge">Are you employed?</p>
+              <p className="text-3xl text-wenge">Are you employed?</p>
               <div className="flex items-center space-x-4">
                 <div className="flex flex-col">
                   <label className="flex items-center">
                     <input type="radio" name="employment" value="yes" className="p-2 rounded-lg" />
-                    <span className="text-wenge ml-2">Yes</span>
+                    <span className="text-wenge ml-2 text-2xl ">Yes</span>
                   </label>
                   <label className="flex items-center">
                     <input type="radio" name="employment" value="no" className="p-2 rounded-lg" />
-                    <span className="ml-2 text-wenge">No</span>
+                    <span className="ml-2 text-wenge text-2xl">No</span>
                   </label>
                 </div>
               </div>
-              <p className="text-2xl text-wenge">Yearly Income</p>
+              <p className=" text-wenge text-3xl">Yearly Income</p>
               <input type="text" placeholder="USD Value" className="py-4 border-2 text-wenge drop-shadow-lg border-gray-300 p-2 rounded-lg" />
-              <p className="text-2xl text-wenge">Financial Literacy Level</p>
+              <p className="text-3xl text-wenge">Financial Literacy Level</p>
               <div className="flex items-center space-x-4">
                 <div className="flex flex-col">
                   <label className="flex items-center">
                     <input type="radio" name="financial-literacy" value="beginner" className="p-2 rounded-lg" />
-                    <span className="text-wenge ml-2">Beginner</span>
+                    <span className="text-wenge ml-2 text-2xl">Beginner</span>
                   </label>
                   <label className="flex items-center">
                     <input type="radio" name="financial-literacy" value="intermediate" className="p-2 rounded-lg" />
-                    <span className="ml-2 text-wenge">Intermediate</span>
+                    <span className="ml-2 text-wenge text-2xl">Intermediate</span>
                   </label>
                   <label className="flex items-center">
                     <input type="radio" name="financial-literacy" value="advanced" className="p-2 rounded-lg" />
-                    <span className="ml-2 text-wenge">Advanced</span>
+                    <span className="ml-2 text-wenge text-2xl">Advanced</span>
                   </label>
                 </div>
               </div>
